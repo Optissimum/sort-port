43a4d75e69534b6a17783f90f677ef7262f59fbe
Sebastian MacDonald <optissimum@gmail.com>
2016-06-28 04:08:18 -0400

    Add inital flask_dance support

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
+from flask_dance.contrib.google import make_google_blueprint, google
+
 
 app = Flask(__name__)
+app.secret_key = "supersekrit"
+blueprint = make_google_blueprint(
+    client_id="my-key-here",
+    client_secret="my-secret-here",
+    scope=["profile", "email"]
+)
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
-from app import app
-from flask import render_template, redirect_url
+from app import app, blueprint
+from flask import render_template, redirect
+
+app.register_blueprint(blueprint, url_prefix="/login")
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return render_template('index.html')
+def home():
+    majorLinks = [('#1', 1),
+                  ('#2', 2),
+                  ('#3', 3),
+                  ('#4', 4),]
+    return render_template('index.html', majorLinks=majorLinks)
