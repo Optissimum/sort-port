3b9a6b05850cffd09206bef165b4b368066b7c94
Sebastian MacDonald <optissimum@gmail.com>
2016-06-29 16:05:34 -0400

    Update database to include items and relationships

--- app/models.py
+++ app/models.py
@@ -4,13 +4,24 @@ from flask_dance.consumer.backend.sqla import OAuthConsumerMixin, SQLAlchemyBack
 from flask_login import LoginManager, UserMixin
 
 class User(db.Model, UserMixin):
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(256), unique=True)
     email = db.Column(db.String(256), unique=True)
     password = db.Column(db.String(256), unique=True)
     first_name = db.Column(db.String(256))
     last_name = db.Column(db.String(256))
-        
+
+    def __init__(self, name):
+        self.name = name
+
 class OAuth(db.Model, OAuthConsumerMixin):
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
     user = db.relationship(User)
+
+class Item():
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    description = db.Column(db.String(256))
+    category = db.Column(db.String(256))
+    owner = db.relationship(User)
