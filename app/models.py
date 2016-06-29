f18e53d4d6239f59cc759bba7f88c182bbdead5a
Sebastian MacDonald <optissimum@gmail.com>
2016-06-29 15:54:53 -0400

    Create initial navigation and correct JS locations

--- app/models.py
+++ app/models.py
@@ -6,8 +6,11 @@ from flask_login import LoginManager, UserMixin
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(256), unique=True)
-    # ... other columns as needed
-
+    email = db.Column(db.String(256), unique=True)
+    password = db.Column(db.String(256), unique=True)
+    first_name = db.Column(db.String(256))
+    last_name = db.Column(db.String(256))
+        
 class OAuth(db.Model, OAuthConsumerMixin):
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
     user = db.relationship(User)
