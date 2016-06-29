3427c6a7cf3222571f4981629c3175d70343559c
Sebastian MacDonald <optissimum@gmail.com>
2016-06-29 09:02:37 -0400

    Add initial SQLAlchemy setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask_dance.contrib.google import make_google_blueprint, google
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = "supersekrit"
+
+# Flask-Dance data
+app.secret_key = "qwertyuiop"
 blueprint = make_google_blueprint(
     client_id="my-key-here",
     client_secret="my-secret-here",
     scope=["profile", "email"]
 )
+
+# SQLAlchemy Setup
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+engine = create_engine("postgresql+psycopg2://user:password@/dbname")
+db = SQLAlchemy(app)
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -6,8 +6,4 @@ app.register_blueprint(blueprint, url_prefix="/login")
 @app.route('/')
 @app.route('/index')
 def home():
-    majorLinks = [('#1', 1),
-                  ('#2', 2),
-                  ('#3', 3),
-                  ('#4', 4),]
-    return render_template('index.html', majorLinks=majorLinks)
+    return render_template('index.html')
