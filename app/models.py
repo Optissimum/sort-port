ca35b882543200fa80f414aa733a32862b0dbca0
Sebastian MacDonald <optissimum@gmail.com>
2016-06-29 09:03:03 -0400

    Create initial users

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from app import app, db
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask_dance.consumer.backend.sqla import OAuthConsumerMixin, SQLAlchemyBackend
+from flask_login import LoginManager, UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(256), unique=True)
+    # ... other columns as needed
+
+class OAuth(db.Model, OAuthConsumerMixin):
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    user = db.relationship(User)
