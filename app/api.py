2514eecf3177dd2b70cb4df3e02ecf695215afa7
Sebastian MacDonald <optissimum@gmail.com>
2016-08-06 01:16:21 -0400

    Feature working back-end to API for item handling

--- app/api.py
+++ app/api.py
@@ -20,26 +20,61 @@ def database_session():
 
 
 def getItemId(item):
-    try:
-        return Item.query.filter_by(name=item.name,
-                                    category_name=item.category_name).count()
-    except:
-        return False
+    with database_session() as session:
+        session.add(item)
+        try:
+            id = Item.query.filter_by(
+                name=item.name, category_name=item.category_name).first().id
+            session.flush()
+            return id
+        except:
+            return False
+
+
+def getItem(id):
+    with database_session() as session:
+        try:
+            q = Item.query.get(id)
+            output = {
+                'id': q.id,
+                'name': q.name,
+                'category_name': q.category_name,
+                'description': q.description,
+                'user': q.user}
+            return output
+        except:
+            return False
 
 
 def addItem(item):
     with database_session() as session:
-        if Item.query.filter_by(id=getItemId(item)).count():
+        if Item.query.filter_by(name=item.name,
+                                category_name=item.category_name).count():
             return False
         else:
             session.add(item)
             return True
-#
-#
-# def addCategory(name):
-#     with database_session() as session:
-#         if Category.query.filter_by(name=name).count():
-#             return False
-#         else:
-#             session.add(Category(name))
-#             return True
+
+
+def editItem(id, name, desc, category, email):
+    with database_session() as session:
+        newItem = {
+            'name': name,
+            'category_name': category,
+            'description': desc,
+            'user_email': email
+        }
+        if session.query(Item).filter_by(id=id).update(newItem):
+            print addCategory(category)
+            return True
+        else:
+            return False
+
+
+def addCategory(name):
+    with database_session() as session:
+        if Category.query.filter_by(name=name).count():
+            return False
+        else:
+            session.add(Category(name))
+            return True
