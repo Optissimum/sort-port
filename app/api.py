b75710280409c48b7bb28b139e71c711b1cdc9ee
Sebastian MacDonald <optissimum@gmail.com>
2016-08-06 02:40:28 -0400

    Add viewCategories and deleteItem features

--- app/api.py
+++ app/api.py
@@ -40,7 +40,7 @@ def getItem(id):
                 'name': q.name,
                 'category_name': q.category_name,
                 'description': q.description,
-                'user': q.user}
+                'user': q.user_email}
             return output
         except:
             return False
@@ -71,6 +71,16 @@ def editItem(id, name, desc, category, email):
             return False
 
 
+def deleteItem(id):
+    with database_session() as session:
+        try:
+            session.delete(Item.query.get(id))
+            return True
+        except Exception as e:
+            raise
+            return False
+
+
 def addCategory(name):
     with database_session() as session:
         if Category.query.filter_by(name=name).one():
@@ -78,6 +88,12 @@ def addCategory(name):
         else:
             session.add(Category(name))
             return True
+
+
+def viewCategories():
+    with database_session() as session:
+        return [category.name for category in Category.query.all()]
+
 
 def addUser(name, email):
     with database_session() as session:
